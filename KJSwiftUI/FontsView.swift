3f58c73bcb45f1bae8e4cea91f6c47d0f00d83be
Kristopher Johnson <kris@kristopherjohnson.net>
2020-01-05 09:35:33 -0500

    Add lorem ipsum to FontsView

--- KJSwiftUI/FontsView.swift
+++ KJSwiftUI/FontsView.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 import UIKit
 
+fileprivate let shortSpecimen = "The quick brown fox jumps over the lazy dog."
+
+fileprivate let loremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
 /// Displays all available fonts in a vertically scrolling view.
 struct FontsView: View {
     private static let fontNames: [String] = {
@@ -18,12 +22,32 @@ struct FontsView: View {
         return names.sorted()
     }()
 
+    @State private var loremIsVisible = false
+
     var body: some View {
         ScrollView {
-            VStack(spacing: 8) {
+            VStack(spacing: 12) {
+                HStack {
+                    Spacer()
+                    Toggle("Show lorem ipsum", isOn: $loremIsVisible)
+                        .fixedSize()
+                    Spacer()
+                }
+                Divider()
                 ForEach(FontsView.fontNames, id: \.self) { fontName in
-                    Text(fontName)
-                        .font(Font.custom(fontName, size: 17))
+                    VStack {
+                        Text(fontName)
+                            .font(.caption)
+                        Text(shortSpecimen)
+                            .font(Font.custom(fontName, size: 20))
+                            .multilineTextAlignment(.center)
+                            .padding(4)
+                        if self.loremIsVisible {
+                            Text(loremIpsum)
+                                .font(Font.custom(fontName, size: 17))
+                                .padding(4)
+                        }
+                    }
                 }
             }
             .padding()
