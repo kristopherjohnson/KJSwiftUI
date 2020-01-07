701ba2403b1e5462021fcf81bc52ebcc94118305
Kristopher Johnson <kris@kristopherjohnson.net>
2020-01-07 06:57:23 -0500

    Add a circular button to ButtonsView

--- KJSwiftUI/ButtonsView.swift
+++ KJSwiftUI/ButtonsView.swift
@@ -10,60 +10,79 @@ import SwiftUI
 /// Displays buttons of various styles.
 struct ButtonsView: View {
     var body: some View {
-        VStack(spacing: 16) {
-            Button(action: {}) {
-                Text("Default Button")
-            }
-
-            Button(action: {}) {
-                Text("Red Button")
-                    .foregroundColor(.red)
-            }
-
-            Button(action: {}) {
-                Text("Bold Button")
-                    .fontWeight(.bold)
-            }
-
-            Button(action: {}) {
-                Text("Fancy Button")
-                    .font(Font.custom("SnellRoundhand-Bold", size: 22))
-            }
-
-            Button(action: {}) {
-                Text("Title Button")
-                    .font(.title)
-            }
-
-            Button(action: {}) {
-                Image(systemName: "magnifyingglass")
-                Text("Search")
-            }
-
-            Button(action: {}) {
-                Text("Bordered Button")
-                    .padding()
-                    .border(Color.accentColor)
-            }
-
-            Button(action: {}) {
-                Text("Filled Rounded Button")
-                    .padding()
-                    .background(Color.accentColor)
-                    .foregroundColor(.white)
-                    .cornerRadius(8)
-            }
-
+        ScrollView(.vertical) {
             HStack {
-                Button(action: {}) {
-                    Image(systemName: "minus.circle")
-                }
-                .padding(.trailing, 8)
-                Button(action: {}) {
-                    Image(systemName: "plus.circle")
-                }
+                Spacer()
+
+                VStack(spacing: 20) {
+                    Button(action: {}) {
+                        Text("Default Button")
+                    }
+
+                    Button(action: {}) {
+                        Text("Red Button")
+                            .foregroundColor(.red)
+                    }
+
+                    Button(action: {}) {
+                        Text("Bold Button")
+                            .fontWeight(.bold)
+                    }
+
+                    Button(action: {}) {
+                        Text("Fancy Button")
+                            .font(Font.custom("SnellRoundhand-Bold", size: 24))
+                    }
+
+                    Button(action: {}) {
+                        Text("Title Button")
+                            .font(.title)
+                    }
+
+                    Button(action: {}) {
+                        Image(systemName: "magnifyingglass")
+                        Text("Search")
+                    }
+
+                    HStack {
+                        Button(action: {}) {
+                            Image(systemName: "minus.circle")
+                        }
+                        .padding(.trailing, 16)
+                        Button(action: {}) {
+                            Image(systemName: "plus.circle")
+                        }
+                    }
+
+                    Button(action: {}) {
+                        Text("Bordered Button")
+                            .padding()
+                            .border(Color.accentColor)
+                    }
+
+                    Button(action: {}) {
+                        Text("Filled Rounded Button")
+                            .padding()
+                            .background(Color.accentColor)
+                            .foregroundColor(.white)
+                            .cornerRadius(8)
+                    }
+
+                    Button(action: {}) {
+                        Circle()
+                            .stroke()
+                            .frame(width: 120, height: 120)
+                            .overlay(
+                                VStack {
+                                    Text("Circular")
+                                    Text("Button")
+                                }
+                            )
+                    }
+                }.padding()
+
+                Spacer()
             }
-            .padding()
         }
     }
 }
