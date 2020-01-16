1dd0f5df634facba01d0a07767ee43c4aa012fa4
Kristopher Johnson <kris@kristopherjohnson.net>
2020-01-15 19:07:28 -0500

    Factor out loremIpsum to its own file

--- KJSwiftUI/FontsView.swift
+++ KJSwiftUI/FontsView.swift
@@ -10,8 +10,6 @@ import UIKit
 
 fileprivate let shortSpecimen = "The quick brown fox jumps over the lazy dog."
 
-fileprivate let loremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-
 /// Displays all available fonts in a vertically scrolling view.
 struct FontsView: View {
     private static let fontNames: [String] = {
