25359db7ec709527a5dbbc2de5a3848abd5d28ab
Kristopher Johnson <kris@kristopherjohnson.net>
2020-01-18 12:55:55 -0500

    Use concentric circles in ShapesView

--- KJSwiftUI/ShapesView.swift
+++ KJSwiftUI/ShapesView.swift
@@ -15,70 +15,80 @@ struct ShapesView: View {
 
                 VStack {
                     Group {
-                        Path { path in
-                            path.move(to: CGPoint(x: 150, y: 0))
-                            path.addLine(to: CGPoint(x: 200, y: 100))
-                            path.addLine(to: CGPoint(x: 120, y: 65))
-                            path.addLine(to: CGPoint(x: 0, y: 80))
-                            path.addLine(to: CGPoint(x: 40, y: 20))
-                            path.closeSubpath()
+                        Path { p in
+                            p.move(to: CGPoint(x: 150, y: 0))
+                            p.addLine(to: CGPoint(x: 200, y: 100))
+                            p.addLine(to: CGPoint(x: 120, y: 65))
+                            p.addLine(to: CGPoint(x: 0, y: 80))
+                            p.addLine(to: CGPoint(x: 40, y: 10))
+                            p.closeSubpath()
                         }
                         .stroke(Color.red, lineWidth: 3)
                         .frame(width: 208, height: 108)
-                        Text("Path")
-                            .font(.caption)
-                            .padding(.bottom)
+
+                        Caption(label: "Path")
+                    }
+
+                    Group {
+                        // Concentric circles
+                        ZStack {
+                            Circle()
+                                .frame(width: 100, height: 100)
+                                .foregroundColor(.red)
+                            Circle()
+                                .frame(width: 66, height: 66)
+                                .foregroundColor(.white)
+                            Circle()
+                                .frame(width: 33, height: 33)
+                                .foregroundColor(.blue)
+                        }
+
+                        Caption(label: "Circles")
                     }
 
                     Group {
                         Rectangle()
                             .frame(width: 200, height: 100)
                             .foregroundColor(.blue)
-                        Text("Rectangle")
-                            .font(.caption)
-                            .padding(.bottom)
+                        Caption(label: "Rectangle")
                     }
 
                     Group {
                         RoundedRectangle(cornerRadius: 16)
                             .frame(width: 200, height: 100)
                             .foregroundColor(.orange)
-                        Text("RoundedRectangle")
-                            .font(.caption)
-                            .padding(.bottom)
-                    }
-
-                    Group {
-                        Circle()
-                            .frame(width: 100, height: 100)
-                            .foregroundColor(.green)
-                        Text("Circle")
-                            .font(.caption)
-                            .padding(.bottom)
+                        Caption(label: "RoundedRectangle")
                     }
 
                     Group {
                         Ellipse()
                             .frame(width: 200, height: 100)
                             .foregroundColor(.purple)
-                        Text("Ellipse")
-                            .font(.caption)
-                            .padding(.bottom)
+                        Caption(label: "Ellipse")
                     }
 
                     Group {
                         Capsule()
                             .frame(width: 200, height: 100)
                             .foregroundColor(.red)
-                        Text("Capsule")
-                            .font(.caption)
-                            .padding(.bottom)
+                        Caption(label: "Capsule")
                     }
                 }.padding()
 
                 Spacer()
             }
         }
+    }
+}
+
+/// Each shape has a text caption beneath it.
+fileprivate struct Caption: View {
+    let label: String
+
+    var body: some View {
+        Text(label)
+            .font(.caption)
+            .padding(.bottom)
     }
 }
 
