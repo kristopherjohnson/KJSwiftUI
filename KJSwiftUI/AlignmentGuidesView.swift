86a85d11daf585bdb38cc38676a5b38564f70336
Kristopher Johnson <kris@kristopherjohnson.net>
2020-01-19 09:23:29 -0500

    Add custom alignment example to AlignmentGuidesView

--- KJSwiftUI/AlignmentGuidesView.swift
+++ KJSwiftUI/AlignmentGuidesView.swift
@@ -9,45 +9,47 @@ import SwiftUI
 
 struct AlignmentGuidesView: View {
     var body: some View {
-        ScrollView(.vertical, showsIndicators: true) {
-            VStack(alignment: .center, spacing: 0) {
+        ScrollView {
+
+            // Horizontal alignment of elements in a VStack
+            VStack(alignment: .center, spacing: 2) {
                 Text("Horizontal Alignment")
                     .padding()
                 Rectangle()
-                    .frame(width: 50, height: 25)
+                    .frame(width: 50, height: 20)
                     .alignmentGuide(HorizontalAlignment.center) { d in
                         d[.leading]
                 }
                 Rectangle()
-                    .frame(width: 75, height: 25)
+                    .frame(width: 75, height: 20)
                     .alignmentGuide(HorizontalAlignment.center) { d in
                         d[.trailing]
                 }
                 Rectangle()
-                    .frame(width: 35, height: 25)
+                    .frame(width: 35, height: 20)
                     .alignmentGuide(HorizontalAlignment.center) { d in
                         d[.leading]
                 }
                 Rectangle()
-                    .frame(width: 15, height: 25)
+                    .frame(width: 15, height: 20)
                     .alignmentGuide(HorizontalAlignment.center) { d in
                         d[.trailing]
                 }
                 Rectangle()
-                    .frame(width: 25, height: 25)
+                    .frame(width: 25, height: 20)
                     .alignmentGuide(HorizontalAlignment.center) { d in
                         d[.leading]
                 }
                 Rectangle()
-                    .frame(width: 60, height: 25)
+                    .frame(width: 60, height: 20)
                     .alignmentGuide(HorizontalAlignment.center) { d in
                         d[.trailing]
                 }
             }
             .foregroundColor(.orange)
-            .padding()
 
-            HStack(alignment: .center, spacing: 0) {
+            // Vertical alignment of elements in an HStack
+            HStack(alignment: .center, spacing: 2) {
                 Text("Vertical Alignment")
                     .padding()
                 Rectangle()
@@ -71,7 +73,7 @@ struct AlignmentGuidesView: View {
                         d[.top]
                 }
                 Rectangle()
-                    .frame(width: 25, height: 25)
+                    .frame(width: 25, height: 20)
                     .alignmentGuide(VerticalAlignment.center) { d in
                         d[.bottom]
                 }
@@ -82,9 +84,63 @@ struct AlignmentGuidesView: View {
                 }
             }
             .foregroundColor(.green)
-            .padding()
+
+            // Custom alignment aligns labels along their trailing edges
+            VStack(alignment: .customLabelAlignment) {
+                Text("Custom Alignment")
+                    .padding()
+                HStack(alignment: .center, spacing: 8) {
+                    Text("First Label")
+                        .bold()
+                        .alignmentGuide(.customLabelAlignment) { d in
+                            d[HorizontalAlignment.trailing]
+                        }
+                    Text("First Value")
+                        .italic()
+                }
+                HStack(alignment: .center, spacing: 8) {
+                    Text("Second Label")
+                        .bold()
+                        .alignmentGuide(.customLabelAlignment) { d in
+                            d[HorizontalAlignment.trailing]
+                        }
+                    Text("Second Value")
+                        .italic()
+                }
+                HStack(alignment: .center, spacing: 8) {
+                    Text("Third Label")
+                        .bold()
+                        .alignmentGuide(.customLabelAlignment) { d in
+                            d[HorizontalAlignment.trailing]
+                        }
+                    Text("Third Value")
+                        .italic()
+                }
+                HStack(alignment: .center, spacing: 8) {
+                    Text("Another Label")
+                        .bold()
+                        .alignmentGuide(.customLabelAlignment) { d in
+                            d[HorizontalAlignment.trailing]
+                        }
+                    Text("Another Value")
+                        .italic()
+                }
+            }
+            .foregroundColor(.blue)
         }
     }
+}
+
+extension HorizontalAlignment {
+
+    /// Custom alignment for trailing edges of labels in AlignmentGuidesView.
+    fileprivate enum CustomLabelAlignment : AlignmentID {
+        static func defaultValue(in context: ViewDimensions) -> CGFloat {
+            return context[HorizontalAlignment.center]
+        }
+    }
+
+    fileprivate static let customLabelAlignment = HorizontalAlignment(CustomLabelAlignment.self)
 }
 
 struct AlignmentGuidesView_Previews: PreviewProvider {
