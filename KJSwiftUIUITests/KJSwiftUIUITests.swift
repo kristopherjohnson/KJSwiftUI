6f7e1fb2c8577d99bc91d9a34a4d289c6634ad4a
Kristopher Johnson <kris@kristopherjohnson.net>
2020-07-13 15:35:39 -0400

    Add simple "click button" UI test

--- KJSwiftUIUITests/KJSwiftUIUITests.swift
+++ KJSwiftUIUITests/KJSwiftUIUITests.swift
@@ -29,8 +29,10 @@ class KJSwiftUIUITests: XCTestCase {
 
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        app.buttons["Buttons"].tap()
     }
 
+/*
     func testLaunchPerformance() {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
             // This measures how long it takes to launch your application.
@@ -39,4 +41,5 @@ class KJSwiftUIUITests: XCTestCase {
             }
         }
     }
+*/
 }
